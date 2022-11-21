ab3b1180a38fb8dcdbf5c74893b5d25079402921
ajvikranth <aj.vikranth@gmail.com>
2022-11-21 15:41:38 +0530

    renaming about 1/2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
-import About from '../components/About'
+import About from '../components/aAbout'
 import Project from '../components/Project'
 import styles from '../styles/Home.module.css'
 import React, { useState } from "react";
