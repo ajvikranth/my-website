c0bad1ec18b0ded2b4e84b576d91532a121a2f31
ajvikranth <aj.vikranth@gmail.com>
2023-01-30 17:04:40 +0530

    adding env

--- pages/p1.js
+++ pages/p1.js
@@ -39,15 +39,16 @@ export default function About() {
       e.preventDefault()
   
       try {
-        const res = await fetch(process.env.OCI_URL, {
+        const oci_url=process.env.OCI_URL;
+        const res = await fetch(oci_url, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(formData)
         });
-        console.log(formData)
+        
         const json = await res.json();
         setApiData(json);
-        console.log(json);
+        
       } catch (err) {
         console.error(err)
       }
@@ -86,9 +87,9 @@ export default function About() {
          makes it easily accessible and convenient for users. This project has been a valuable learning experience, honing my skills in 
          data science and machine learning. 
       </p>
-        <a href="https://www.kaggle.com/datasets/saisaathvik/used-cars-dataset-from-cardekhocom?select=cardekho_updated.csv" class="inline-flex items-center text-blue-600 hover:underline text-2xl">
+        <a href="https://www.kaggle.com/datasets/saisaathvik/used-cars-dataset-from-cardekhocom?select=cardekho_updated.csv" className="inline-flex items-center text-blue-600 hover:underline text-2xl">
         dataset source
-        <svg class="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+        <svg className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
         </a>
       
       
